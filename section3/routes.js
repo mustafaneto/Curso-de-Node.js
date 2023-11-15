580692a1f64a215268557f243afc2f145fee769d
Mustafa Neto <mustafaneto26@gmail.com>
2023-11-15 13:18:30 -0300

    debugging

--- section3/routes.js
+++ section3/routes.js
@@ -20,6 +20,7 @@ const requestHandler = (req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
+    
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
